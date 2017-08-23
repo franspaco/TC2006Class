a347d4fc0cb3347aaad1383a0726444415532a0e
franspaco <franspaco@gmail.com>
2017-08-23 13:11:20 -0500

    Version de la clase

--- Practicas/Ejercicios/Clase2.hs
+++ Practicas/Ejercicios/Clase2.hs
@@ -11,3 +11,10 @@ minMax (h:t) = mm (h, h) t
                         | head arr > fst tup  = mm (head arr, snd tup) (drop 1 arr)
                         | head arr < snd tup  = mm (fst tup, head arr) (drop 1 arr)
                         | otherwise           = mm tup (drop 1 arr)
+
+minMax2 x = mM (tail x) (head x) (head x)
+                where mM x maxx minn
+                        | x == []         = (maxx, minn)
+                        | (head x) > maxx = mM (tail x) (head x) minn
+                        | (head x) < minn = mM (tail x) maxx (head x)
+                        | otherwise       = mM (tail x) maxx minn
