e3e82572d244036be8fbb9fd168f254fcc84d3bf
franspaco <franspaco@gmail.com>
2017-11-15 11:55:52 -0600

    dif list

---
+++ prolog/difList.pl
@@ -0,0 +1,6 @@
+
+myappend(A,B,B,A).
+
+myap1(A-B, B, A).
+
+myap2(A-B, B-C, A-C).
--- prolog/test.pl
+++
@@ -1,9 +0,0 @@
-partition([X|Xs],Y,[X|Ls],Rs):- X =< Y, partition(Xs,Y,Ls,Rs).
-partition([X|Xs],Y,Ls,[X|Rs]):- X > Y, partition(Xs,Y,Ls,Rs).
-partition([], Y, [], []).
-
-partition2(PIVOT, [], [], []).
-partition2(PIVOT, [HEAD | TAIL], [HEAD | LEFT], RIGHT) :- HEAD @=< PIVOT,
-                                                         partition2(PIVOT, TAIL, LEFT, RIGHT).
-partition2(PIVOT, [HEAD | TAIL], LEFT, [HEAD | RIGHT]) :- HEAD @> PIVOT,
-                                                         partition2(PIVOT, TAIL, LEFT, RIGHT).
